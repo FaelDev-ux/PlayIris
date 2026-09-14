75f17f0a9ca5710d36786263d1415f82915c7f1a
Rubens Rafael • Fael Dev <tiorafa2006@gmail.com>
2026-09-14 11:42:16 -0300

    :lipstick: feat: aplica paleta Íris

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import './style.css';
+
+const app = document.querySelector('#app');
+
+app.innerHTML = `
+  <h1 class=" text-center text-4xl font-bold text-lilas-cognitivo">PlayIris</h1>
+  <p class=" font-bold text-4xl rounded-2xl bg-branco-porcelana p-6 shadow-lg text-center">Projeto funfando com Vite</p>
+`;
