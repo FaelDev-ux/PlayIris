06afb36bd07e414fa32d89501b89342fa9cbb565
samuelmedeirosjs <samuel.medeiros014@gmail.com>
2026-09-25 07:13:25 -0300

    :wrench::bug:raw: adiciona force reload automático no modo dev

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
+const forceReload = () => ({
+  name: 'force-reload',
+  handleHotUpdate({ file, server }) {
+    if (file.endsWith('.js') || file.endsWith('.jsx')) {
+      server.ws.send({ type: 'full-reload' });
+      return [];
+    }
+  }
+});
+
 export default defineConfig({
-  plugins: [tailwindcss()],
+  plugins: [tailwindcss(), forceReload()],
   esbuild: {
     jsx: 'transform',
     jsxInject: `import * as h from 'jsx-dom'`,
